c94f6a1f9e08e0478610b0b7f08430f6fd0e11ce
ssoonan <ssoonan0770@gmail.com>
2025-01-06 16:16:35 +0900

    Config::build, run 함수 분리, 에러 처리 구현

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,13 +1,45 @@
 use ::std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    // config, args의 에러 발생 가능성
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    let query = &args[1];
-    let file_path = &args[2];
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+    // fs::read_to_string의 에러 발생 가능성
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
 
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
     println!("With text:\n{contents}");
+
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    file_path: String, // 귀찮지만 일단 str은 &str로만 쓰고 보통 타입으론 소유권이 있는 String을 쓴다고 보기
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Ok(Config { query, file_path })
+    }
 }
